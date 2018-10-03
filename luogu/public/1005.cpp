5155aa666dc81e6e2415f2d7be84fe470cfcba79
Polaris1949 <Polaris1949@126.com>
2018-10-03 19:05:34 +0800

    Update; need revision

--- luogu/public/1005.cpp
+++ luogu/public/1005.cpp
@@ -1,128 +1,138 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cmath>
-using namespace std;
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
 
-struct bign
+class big
 {
-	int l;
-	short int w[2000];
-	bign()
-	{
-		l=1;
-		memset(w,0,sizeof(w));
-	}
+public:
+std::vector<short> num;
 
-	bign(int x)
-	{
-		l=0;
-		memset(w,0,sizeof(w));
-		while(x)
-		{
-			w[l]=x%10,x/=10;
-			l++;
-		}
-	}
+big()
+{
+  num.push_back(0);
+}
 
-	bool operator >(bign x)
-	{
-		if(l>x.l)return 1;
-		if(l==x.l)
-		{
-			for(int i=l-1; i>=0; i--)
-			{
-				if(w[i]<x.w[i])return 0;
-				if(w[i]>x.w[i])return 1;
-			}
-		}
-		return 0;
-	}
+big(int x)
+{
+  while (x)
+    {
+      num.push_back(x % 10);
+      x /= 10;
+    }
+}
 
-	bign operator +(const bign& x)
-	{
-		bign ans;
-		ans.l=max(l,x.l);
+bool operator <(const big& x) const
+{
+  if (len == x.len)
+    {
+      for (size_t i = len - 1; i >= 0; i--)
+        {
+          if (num[i] < x.num[i])
+            return 0;
+          if (num[i] > x.num[i])
+            return 1;
+        }
+    }
+else return len < x.len;
+}
 
-		for(int i=0; i<ans.l; i++)
-		{
-			ans.w[i]+=w[i]+x.w[i];
-			ans.w[i+1]+=ans.w[i]/10;
-			ans.w[i]%=10;
-		}
+big operator +(const big& x)
+{
+  big ans;
 
-		if(ans.w[ans.l])ans.l++;
-		return ans;
-	}
+  ans.len = std::max(len, x.len);
 
-	bign operator *(const bign& x)
-	{
-		bign ans;
-		ans.l=l+x.l;
+  for (int i = 0; i < ans.len; i++)
+    {
+      ans.num[i] += num[i] + x.num[i];
+      ans.num[i + 1] += ans.num[i] / 10;
+      ans.num[i] %= 10;
+    }
 
-		for(int i=0; i<l; i++)
-		{
-			for(int j=0; j<x.l; j++)
-			{
-				ans.w[i+j]+=w[i]*x.w[j];
-				ans.w[i+j+1]+=ans.w[i+j]/10;
-				ans.w[i+j]%=10;
-			}
-		}
+  if (ans.num[ans.len])
+    ans.len++;
+  return ans;
+}
 
-		if(ans.w[ans.l-1]==0)ans.l--;
-		return ans;
-	}
+big operator *(const big& x)
+{
+  big ans;
 
-	bign operator *(int x)
-	{
-		return (*this)*(bign(x));
-	}
+  ans.len = len + x.len;
 
-	void write()
-	{
-		if(l==0)printf("0");
-		for(int i=l-1; i>=0; i--)printf("%d",w[i]);
-		printf("\n");
-	}
+  for (int i = 0; i < len; i++)
+    {
+      for (int j = 0; j < x.len; j++)
+        {
+          ans.num[i + j] += num[i] * x.num[j];
+          ans.num[i + j + 1] += ans.num[i + j] / 10;
+          ans.num[i + j] %= 10;
+        }
+    }
+
+  if (ans.num[ans.len - 1] == 0)
+    ans.len--;
+  return ans;
+}
+
+big operator *(int x)
+{
+  return (*this) * (big(x));
+}
+
+void write()
+{
+  if (len == 0)
+    printf("0");
+  for (int i = len - 1; i >= 0; i--)
+    printf("%d", num[i]);
+  printf("\n");
+}
 };
 
-int n,m;
+int n, m;
 int a[85];
-bign ans;
-bign f[85][85];
-bign tm[85];
+big ans;
+big f[85][85];
+big tm[85];
 
 int main()
 {
-	int i,j,k;
-	scanf("%d%d",&n,&m);
+  int i, j, k;
+  std::cin >> n >> m;
 
-	tm[0]=1;
-	for(i=1; i<=m; i++)tm[i]=tm[i-1],tm[i]=tm[i]*2;
+  tm[0] = 1;
+  for (i = 1; i <= m; i++)
+    tm[i] = tm[i - 1], tm[i] = tm[i] * 2;
 
-	ans=0;
-	for(i=1; i<=n; i++)
-	{
-		for(j=1; j<=m; j++)scanf("%d",&a[j]);
+  ans = 0;
+  for (i = 1; i <= n; i++)
+    {
+      for (j = 1; j <= m; j++)
+        std::cin >> a[j];
 
-		for(j=1; j<=m; j++)f[j][j]=tm[m]*a[j];
+      for (j = 1; j <= m; j++)
+        f[j][j] = tm[m] * a[j];
 
-		for(j=1; j<m; j++)
-		{
-			for(k=1; k<m; k++)
-			{
-				if(j+k>m)break;
-				bign t1=tm[m-j]*a[k]+f[k+1][k+j];
-				bign t2=tm[m-j]*a[k+j]+f[k][k+j-1];
-				if(t1>t2)f[k][k+j]=t1;
-				else f[k][k+j]=t2;
-			}
-		}
+      for (j = 1; j < m; j++)
+        {
+          for (k = 1; k < m; k++)
+            {
+              if (j + k > m)
+                break;
+              big t1 = tm[m - j] * a[k] + f[k + 1][k + j];
+              big t2 = tm[m - j] * a[k + j] + f[k][k + j - 1];
+              if (t1 > t2)
+                f[k][k + j] = t1;
+              else
+                f[k][k + j] = t2;
+            }
+        }
 
-		ans=ans+f[1][m];
-	}
+      ans = ans + f[1][m];
+    }
 
-	ans.write();
-	return 0;
+  ans.write();
+  return 0;
 }
