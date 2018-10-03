1a1c506958c3ea83714cfe14220bc224bb6eadc9
Polaris1949 <Polaris1949@126.com>
2018-10-03 17:59:37 +0800

    Update

--- luogu/public/1002.cpp
+++ luogu/public/1002.cpp
@@ -1,26 +1,30 @@
 #include <cmath>
 #include <iostream>
 
-long long f[30][30];
+long long f[22][22];
+
+inline long long g(int x, int y)
+{
+  return x < 0 ? y == 0 : f[x][y];
+}
 
 int main()
 {
   int n, m, x, y, i, j;
   std::cin >> n >> m >> x >> y;
 
-  ++n; ++m; ++x; ++y;
-  f[0][1] = 1;
+  ++n; ++m;
 
-  for (i = 1; i <= n; ++i)
-    for (j = 1; j <= m; ++j)
+  for (i = 0; i < n; ++i)
+    for (j = 0; j < m; ++j)
       {
-        if ((abs(i - x) + abs(j - y) == 3) && (i != x) && (j != y)
+        if (((abs(i - x) + abs(j - y) == 3) && (i != x) && (j != y))
             || (i == x && j == y))
           continue;
         else
-          f[i][j] = f[i - 1][j] + f[i][j - 1];
+          f[i][j] = g(i - 1, j) + g(i, j - 1);
       }
 
-  std::cout << f[n][m] << '\n';
+  std::cout << f[n - 1][m - 1] << '\n';
   return 0;
 }
