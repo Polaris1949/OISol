b0f89f6537dfc153b1dc9e1f45cf30a974b1e6bb
Polaris1949 <Polaris1949@126.com>
2019-07-02 12:28:33 +0800

    Remove

--- luogu/dev/lcv.cpp
+++
@@ -1,8 +0,0 @@
-#include "luogu.hpp"
-
-int main()
-{
-    LG_SYS("cat /proc/version");
-    // Expected result:
-    // Linux version 4.9.116-43.59.amzn1.x86_64 (mockbuild@gobi-build-64012) (gcc version 7.2.1 20170915 (Red Hat 7.2.1-2) (GCC) ) #1 SMP Thu Aug 2 23:15:06 UTC 2018
-}
